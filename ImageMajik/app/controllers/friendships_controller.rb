669194de4c157c2f66e3cdff2ae1ec81e52091fa
doods103 <doods103@hotmail.com>
2014-10-19 19:56:28 +1100

    links added
    links to decline request added and links to remove friend added but they
    dont work :(

--- ImageMajik/app/controllers/friendships_controller.rb
+++ ImageMajik/app/controllers/friendships_controller.rb
@@ -4,6 +4,8 @@ before_filter :authenticate_user!
 
   def index
     @friends = current_user.friends
+    @pending_invited_by = current_user.pending_invited_by
+    @pending_invited = current_user.pending_invited
   end
 
   def new
