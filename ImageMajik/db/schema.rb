bd9bdeeb2484305ff1c6cdfadb59f6b273d3e67d
AlgoRyan <contact.hodgman.r@gmail.com>
2014-10-19 14:20:16 +1100

    Got friendships working.
    Can now successfully add a friend! Created some dummy placeholders for
    user profile images for the moment to show functionality.

--- ImageMajik/db/schema.rb
+++ ImageMajik/db/schema.rb
@@ -11,18 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016125748) do
+ActiveRecord::Schema.define(version: 20141019025301) do
 
   create_table "albums", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.integer  "album_id"
   end
 
-  add_index "albums", ["album_id"], name: "index_albums_on_album_id"
   add_index "albums", ["user_id"], name: "index_albums_on_user_id"
+
+  create_table "friendships", force: true do |t|
+    t.integer "friendable_id"
+    t.integer "friend_id"
+    t.integer "blocker_id"
+    t.boolean "pending",       default: true
+  end
+
+  add_index "friendships", ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
 
   create_table "images", force: true do |t|
     t.string   "label"
@@ -51,7 +58,6 @@ ActiveRecord::Schema.define(version: 20141016125748) do
     t.string   "account_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
